e703d8d312fc95271ce95f881b3d7f29e34d54b1
Natalee Zatkoff <nataleezatkoff@gmail.com>
2023-05-13 14:53:16 -0400

    added a drive controller to drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 // Imports
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Drivetrain {
@@ -14,8 +16,9 @@ public class Drivetrain {
     private CANSparkMax FRMotor;
     private CANSparkMax RLMotor;
     private CANSparkMax RRMotor; 
-    MotorControllerGroup leftMotors;
-    MotorControllerGroup rightMotors;
+    private MotorControllerGroup leftMotors;
+    private MotorControllerGroup rightMotors;
+    private DifferentialDrive driveController;
 
     // Constructor method
     public Drivetrain( int FLMotorID, int FRMotorID, int RLMotorID, int RRMotorID ) {
@@ -32,15 +35,27 @@ public class Drivetrain {
         // Create groups
         leftMotors = new MotorControllerGroup(FLMotor, RLMotor);
         rightMotors = new MotorControllerGroup(FRMotor, RRMotor);
+
+        // Initialize Differentials Drive
+        driveController = new DifferentialDrive(leftMotors, rightMotors);
     }
 
     // Accessor methods (getters)
-    public CANSparkMax getFLMotor() { return this.FLMotor; }
-    public CANSparkMax getFRMotor() { return this.FRMotor; }
-    public CANSparkMax getRLMotor() { return this.RLMotor; }
-    public CANSparkMax getRRMotor() { return this.RRMotor; }
-    public MotorControllerGroup getRightMotorGroup() { return this.rightMotors; }
-    public MotorControllerGroup getLeftMotorGroup() { return this.leftMotors; }
+    public CANSparkMax getFLMotor() { return FLMotor; }
+    public CANSparkMax getFRMotor() { return FRMotor; }
+    public CANSparkMax getRLMotor() { return RLMotor; }
+    public CANSparkMax getRRMotor() { return RRMotor; }
+    public MotorControllerGroup getRightMotorGroup() { return rightMotors; }
+    public MotorControllerGroup getLeftMotorGroup() { return leftMotors; }
+    public DifferentialDrive getDriveController() { return driveController; }
+
+    /** Drives the robot by controlling the speed and rotation
+     * @param driveSpeed Drives the robot forward and back [-1,1]
+     * @param rotationSpeed Spins the robot left and right [-1,1]
+     */
+    public void arcadeDrive(double driveSpeed, double rotationSpeed) {
+        driveController.arcadeDrive(driveSpeed, rotationSpeed);
+    }
 
 }
 
