3c0d02be656c3c89a16e65fb48efdb70662089b7
Natalee Zatkoff <nataleezatkoff@gmail.com>
2023-05-13 15:08:40 -0400

    added the motor ids in constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,11 +12,14 @@ public final class Constants {
 
     /** Ports used by controllers. */
     public static final class ControllerPorts {
-
+        public static final int CONTROLLER = 0;
     }
 
     /** Motor IDs used by the drivetrain. */
     public static final class DrivetrainIDs {
-
+        public static final int FL_MOTOR_ID = 7;
+        public static final int FR_MOTOR_ID = 3;
+        public static final int RL_MOTOR_ID = 6;
+        public static final int RR_MOTOR_ID = 2;
     }
 } 
